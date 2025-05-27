1c183156a4d7ffee7927be83c9dc43185245cc36
VolodymyrHasyn <phenom2405@gmail.com>
2025-05-27 21:07:22 +0300

    build

--- dist/steps/login.steps.js
+++ dist/steps/login.steps.js
@@ -10,6 +10,13 @@ const test_1 = require("@playwright/test");
     //   await this.page.fill('#password', 'password');
     await this.page.click('#contact-us-btn-header');
 });
+(0, cucumber_1.When)('I get some response', async function () {
+    const response = await this.request.get(`https://luxequality.com/`);
+    this.response = response;
+});
+(0, cucumber_1.Then)('I should receive response {int}', async function (statusCode) {
+    await (0, test_1.expect)(this.response.status()).toBe(statusCode);
+});
 (0, cucumber_1.Then)('I should be redirected to the dashboard', async function () {
     await (0, test_1.expect)(this.page).toHaveURL('https://luxequality.com/contacts/');
 });
