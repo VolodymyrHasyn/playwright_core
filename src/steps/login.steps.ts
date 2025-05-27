4b4e900fd28b2b7e317ad7516a3564241be7ff9c
VolodymyrHasyn <phenom2405@gmail.com>
2025-05-27 20:30:15 +0300

    new commit

--- src/steps/login.steps.ts
+++ src/steps/login.steps.ts
@@ -2,15 +2,15 @@ import { Given, When, Then } from '@cucumber/cucumber';
 import { expect } from '@playwright/test';
 
 Given('I open the login page', async function () {
-  await this.page.goto('https://example.com/login');
+  await this.page.goto('https://luxequality.com/');
 });
 
 When('I login with valid credentials', async function () {
-  await this.page.fill('#username', 'user');
-  await this.page.fill('#password', 'password');
-  await this.page.click('button[type="submit"]');
+//   await this.page.fill('#username', 'user');
+//   await this.page.fill('#password', 'password');
+  await this.page.click('#contact-us-btn-header');
 });
 
 Then('I should be redirected to the dashboard', async function () {
-  await expect(this.page).toHaveURL(/.*dashboard/);
+  await expect(this.page).toHaveURL('https://luxequality.com/contacts/');
 });
