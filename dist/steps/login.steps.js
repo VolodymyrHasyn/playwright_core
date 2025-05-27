c0bb42cc52d8ae1564bcd23fb51a005df4e28b82
VolodymyrHasyn <phenom2405@gmail.com>
2025-05-27 20:43:19 +0300

    added

--- dist/steps/login.steps.js
+++ dist/steps/login.steps.js
@@ -3,13 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const cucumber_1 = require("@cucumber/cucumber");
 const test_1 = require("@playwright/test");
 (0, cucumber_1.Given)('I open the login page', async function () {
-    await this.page.goto('https://example.com/login');
+    await this.page.goto('https://luxequality.com/');
 });
 (0, cucumber_1.When)('I login with valid credentials', async function () {
-    await this.page.fill('#username', 'user');
-    await this.page.fill('#password', 'password');
-    await this.page.click('button[type="submit"]');
+    //   await this.page.fill('#username', 'user');
+    //   await this.page.fill('#password', 'password');
+    await this.page.click('#contact-us-btn-header');
 });
 (0, cucumber_1.Then)('I should be redirected to the dashboard', async function () {
-    await (0, test_1.expect)(this.page).toHaveURL(/.*dashboard/);
+    await (0, test_1.expect)(this.page).toHaveURL('https://luxequality.com/contacts/');
 });
